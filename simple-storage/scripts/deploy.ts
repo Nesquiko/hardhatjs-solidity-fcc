63b6905ef4d37856494d8f55219d50ded511087c
Nesquiko <lukas.castven@gmail.com>
2022-12-09 15:19:24 +0100

    look on stuff

--- simple-storage/scripts/deploy.ts
+++ simple-storage/scripts/deploy.ts
@@ -1,15 +1,17 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-	const ssFact = await ethers.getContractFactory('SimpleStorage')
+  const ssFact = await ethers.getContractFactory('SimpleStorage')
 
-	const simpleStorage = await ssFact.deploy()
-	await simpleStorage.deployed()
+  const simpleStorage = await ssFact.deploy()
+  await simpleStorage.deployed()
 }
 
+async function verify(addr: string, args: any[]) {}
+
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error)
-		process.exitCode = 1
-	})
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+  })
